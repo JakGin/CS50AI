8c3fed83f2d71fdb611915fb1c76cc18f53b837b
Jakub Glinka <jakubglinka9@gmail.com>
2024-07-10 13:56:08 +0200

    Implement pagerank algorithm

--- 2_Uncertainty/pagerank/pagerank.py
+++ 2_Uncertainty/pagerank/pagerank.py
@@ -48,7 +48,7 @@ def crawl(directory):
     return pages
 
 
-def transition_model(corpus, page, damping_factor):
+def transition_model(corpus: dict[str, set[str]], page: str, damping_factor: float):
     """
     Return a probability distribution over which page to visit next,
     given a current page.
@@ -57,10 +57,19 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    links = corpus[page]
+    if len(links) == 0:
+        links_probability = 0
+    else:
+        links_probability = damping_factor / len(links)
+    all_probability = (1 - damping_factor) / len(corpus)
+    return {
+        key: links_probability + all_probability if key in links else all_probability
+        for key in corpus
+    }
 
 
-def sample_pagerank(corpus, damping_factor, n):
+def sample_pagerank(corpus: dict[str, set[str]], damping_factor: float, n: int):
     """
     Return PageRank values for each page by sampling `n` pages
     according to transition model, starting with a page at random.
@@ -69,10 +78,37 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+    def add_sample(page):
+        if page not in samples:
+            samples[page] = 1
+        else:
+            samples[page] += 1
+
+    # samples is a dict having as key the page (e.g. 1.html)
+    # and as a value the amount of times this page was chosen
+    samples = {}
+
+    # Choose page at random and add to samples
+    pages = list(corpus.keys())
+    page = random.choice(pages)
+    add_sample(page)
+
+    # Loop through every sample chosing next page based on transitional model
+    for _ in range(n - 1):
+        distribution = transition_model(corpus, page, damping_factor)
+        page = random.choices(list(distribution.keys()), 
+                              weights=list(distribution.values()), k=1)[0]
+        add_sample(page)
+
+    # Normalize samples to sum up to 1
+    total_sum = sum(samples.values())
+    normalized_samples = {key: value / total_sum for key, value in samples.items()}
+    
+    return normalized_samples
 
 
-def iterate_pagerank(corpus, damping_factor):
+def iterate_pagerank(corpus: dict[str, set[str]], damping_factor: float):
     """
     Return PageRank values for each page by iteratively updating
     PageRank values until convergence.
@@ -81,7 +117,52 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+    # Interprete a page that has no links as page that that
+    # has links to every page including itself
+    corpus = corpus.copy()
+    for key, value in corpus.items():
+        if len(value) == 0:
+            for key in corpus:
+                value.add(key)
+    rev_corpus = links_to_pages(corpus)
+
+    starting_probability = 1 / len(corpus)
+    distribution = {key: starting_probability for key in corpus}
+
+    factor1 = (1 - damping_factor) / len(corpus)
+
+    # Adjust the propability distribution until no PageRank value changes by
+    # more that 0.0001 between the current rank values and the new rank values
+    stop = False
+    while not stop:
+        stop = True
+        for key, value in distribution.items():
+            pages = rev_corpus[key]
+            total_sum = 0
+            for page in pages:
+                total_sum += distribution[page] / len(corpus[page])
+            factor2 = damping_factor * total_sum
+            new_value = factor1 + factor2
+            distribution[key] = new_value
+            if abs(value - new_value) > 0.001:
+                stop = False
+
+    return distribution
+
+
+def links_to_pages(corpus: dict[str, set[str]]):
+    """
+    Return a dict with key, value pairs where key is the page (e.g. 1.html) and value is a set containing pages that link to the page in the key
+    """
+    rev_corpus = {}
+    for page in corpus:
+        links = set()
+        for key, value in corpus.items():
+            if page in value:
+                links.add(key)
+        rev_corpus[page] = links
+    return rev_corpus
 
 
 if __name__ == "__main__":
