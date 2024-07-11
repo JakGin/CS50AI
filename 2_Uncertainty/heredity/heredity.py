4144b6eb27c16108589998c955eda5ae01efbb93
Jakub Glinka <jakubglinka9@gmail.com>
2024-07-11 12:14:58 +0200

    Implement heredity

--- 2_Uncertainty/heredity/heredity.py
+++ 2_Uncertainty/heredity/heredity.py
@@ -139,7 +139,97 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    raise NotImplementedError
+
+    def get_parents_heredity_prob(person: str) -> tuple[float, float, float, float]:
+        """
+        Calculate the probability of getting gene from mother or father based on the rules of heredity and the mutation
+        """
+        mother = people[person]["mother"]
+        father = people[person]["father"]
+        mother_genes = (
+            2 if mother in two_genes else 1 if mother in one_gene else 0
+        )
+        father_genes = (
+            2 if father in two_genes else 1 if father in one_gene else 0
+        )
+
+        mother_prob = 1 - PROBS["mutation"] if mother_genes == 2 \
+            else 0.5 if mother_genes == 1 else PROBS["mutation"]
+        not_mother_prob = PROBS["mutation"] if mother_genes == 2 \
+            else 0.5 if mother_genes == 1 else 1 - PROBS["mutation"]
+        father_prob = 1 - PROBS["mutation"] if father_genes == 2 \
+            else 0.5 if father_genes == 1 else PROBS["mutation"]
+        not_father_prob = PROBS["mutation"] if father_genes == 2 \
+            else 0.5 if father_genes == 1 else 1 - PROBS["mutation"]
+
+        return mother_prob, not_mother_prob, father_prob, not_father_prob
+    
+    probs = []
+
+    for person in people:
+
+        # Person with 1 gene
+        if person in one_gene:
+            # Calculate probability for having 1 gene based on having parents information or not
+            if not people[person]["father"]:
+                gene_prob = PROBS["gene"][1]
+            else:
+                # 2 possibilities - gene from mother or from father
+                mother_prob, not_mother_prob, father_prob, not_father_prob = (
+                    get_parents_heredity_prob(person)
+                )
+                gene_prob = (mother_prob * not_father_prob) + \
+                    (father_prob * not_mother_prob)
+
+            if person in have_trait:
+                trait_prob = PROBS["trait"][1][True]
+            else:
+                trait_prob = PROBS["trait"][1][False]
+
+        # Person with 2 genes
+        elif person in two_genes:
+            # Calculate probability for having 1 gene based on having parents information or not
+            if not people[person]["father"]:
+                gene_prob = PROBS["gene"][2]
+            else:
+                # 1 possiblity - 1 gene from mother and 1 gene from father
+                mother_prob, not_mother_prob, father_prob, not_father_prob = (
+                    get_parents_heredity_prob(person)
+                )
+                gene_prob = mother_prob * father_prob
+
+            if person in have_trait:
+                trait_prob = PROBS["trait"][2][True]
+            else:
+                trait_prob = PROBS["trait"][2][False]
+
+        # Person with 0 genes
+        else:
+            # Calculate probability for having 0 genes based on having parents information or not
+            if not people[person]["father"]:
+                gene_prob = PROBS["gene"][0]
+            else:
+                # 1 possibility - none of parent give a gene
+                mother_prob, not_mother_prob, father_prob, not_father_prob = (
+                    get_parents_heredity_prob(person)
+                )
+                gene_prob = not_mother_prob * not_father_prob
+
+            if person in have_trait:
+                trait_prob = PROBS["trait"][0][True]
+            else:
+                trait_prob = PROBS["trait"][0][False]
+            
+        # Combine probability for a person knowing the probability of having
+        # specific amount of genes and probability of having the trait
+        probs.append(gene_prob * trait_prob)
+                
+    # Calculate joint probability by multiplying values for each of the people
+    joint_probability = 1
+    for prob in probs:
+        joint_probability *= prob
+
+    return joint_probability
 
 
 def update(probabilities, one_gene, two_genes, have_trait, p):
@@ -149,7 +239,18 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    for person in probabilities:
+        if person in one_gene:
+            probabilities[person]["gene"][1] += p
+        elif person in two_genes:
+            probabilities[person]["gene"][2] += p
+        else:
+            probabilities[person]["gene"][0] += p
+
+        if person in have_trait:
+            probabilities[person]["trait"][True] += p
+        else:
+            probabilities[person]["trait"][False] += p
 
 
 def normalize(probabilities):
@@ -157,7 +258,24 @@ def normalize(probabilities):
     Update `probabilities` such that each probability distribution
     is normalized (i.e., sums to 1, with relative proportions the same).
     """
-    raise NotImplementedError
+    for person in probabilities:
+        total_g = (
+            probabilities[person]["gene"][0]
+            + probabilities[person]["gene"][1]
+            + probabilities[person]["gene"][2]
+        )
+        scaling_factor = 1 / total_g
+        probabilities[person]["gene"][0] *= scaling_factor
+        probabilities[person]["gene"][1] *= scaling_factor
+        probabilities[person]["gene"][2] *= scaling_factor
+
+        total_t = (
+            probabilities[person]["trait"][True]
+            + probabilities[person]["trait"][False]
+        )
+        scaling_factor = 1 / total_t
+        probabilities[person]["trait"][True] *= scaling_factor
+        probabilities[person]["trait"][False] *= scaling_factor
 
 
 if __name__ == "__main__":
